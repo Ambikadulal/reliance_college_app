31008595ba811d5dc7f1d88de686c5873b54e571
Nutan kafle <kaflenutan4@123>
2025-04-04 09:37:32 +0545

    change in profile

---
+++ lib/pages/screens/widgets/buttom.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+// ignore: camel_case_types
+class commonbuttomwidget extends StatelessWidget {
+  const commonbuttomwidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
