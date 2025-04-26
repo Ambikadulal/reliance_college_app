0a587593d70e94afad61ec0a96a7e75ecf282e1b
Sudeep <bhattaraisudeep188@gmail.com>
2025-04-26 17:27:31 +0545

    managing height

--- lib/pages/screens/academic_page.dart
+++ lib/pages/screens/academic_page.dart
@@ -5,145 +5,138 @@ class AcademicsSection extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      child: Card(
-        color: const Color(0xFF1E1E1E),
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
-        margin: EdgeInsets.all(20),
-        child: Padding(
-          padding: const EdgeInsets.all(16),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const Text(
-                "Academics",
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold,
-                ),
+    return Container(
+      color: Colors.black87,
+      child: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text(
+              "Academics",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
               ),
-              const SizedBox(height: 16),
-              Expanded(
-                child: GridView.count(
-                  crossAxisCount: 4,
-                  physics: const NeverScrollableScrollPhysics(),
-                  mainAxisSpacing: 20,
-                  crossAxisSpacing: 10,
-                  children: const [
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Icon(Icons.assignment, color: Colors.white, size: 28),
-                        SizedBox(height: 6),
-                        Text(
-                          "Assignment",
-                          style: TextStyle(color: Colors.white, fontSize: 11),
-                          textAlign: TextAlign.center,
-                        ),
-                      ],
-                    ),
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Icon(Icons.schedule, color: Colors.white, size: 28),
-                        SizedBox(height: 6),
-                        Text(
-                          "Routine",
-                          style: TextStyle(color: Colors.white, fontSize: 11),
-                          textAlign: TextAlign.center,
-                        ),
-                      ],
-                    ),
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Icon(
-                          Icons.local_library,
-                          color: Colors.white,
-                          size: 28,
-                        ),
-                        SizedBox(height: 6),
-                        Text(
-                          "Library",
-                          style: TextStyle(color: Colors.white, fontSize: 11),
-                          textAlign: TextAlign.center,
-                        ),
-                      ],
-                    ),
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Icon(Icons.menu_book, color: Colors.white, size: 28),
-                        SizedBox(height: 6),
-                        Text(
-                          "E-Learning",
-                          style: TextStyle(color: Colors.white, fontSize: 11),
-                          textAlign: TextAlign.center,
-                        ),
-                      ],
-                    ),
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Icon(Icons.shopping_bag, color: Colors.white, size: 28),
-                        SizedBox(height: 6),
-                        Text(
-                          "Leave",
-                          style: TextStyle(color: Colors.white, fontSize: 11),
-                          textAlign: TextAlign.center,
-                        ),
-                      ],
-                    ),
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Icon(
-                          Icons.calendar_month_rounded,
-                          color: Colors.white,
-                          size: 28,
-                        ),
-                        SizedBox(height: 6),
-                        Text(
-                          "Calendar",
-                          style: TextStyle(color: Colors.white, fontSize: 11),
-                          textAlign: TextAlign.center,
-                        ),
-                      ],
-                    ),
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Icon(
-                          Icons.bookmark_added_outlined,
-                          color: Colors.white,
-                          size: 28,
-                        ),
-                        SizedBox(height: 6),
-                        Text(
-                          "Syllabus",
-                          style: TextStyle(color: Colors.white, fontSize: 11),
-                          textAlign: TextAlign.center,
-                        ),
-                      ],
-                    ),
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Icon(Icons.today, color: Colors.white, size: 28),
-                        SizedBox(height: 6),
-                        Text(
-                          "Daily Plan",
-                          style: TextStyle(color: Colors.white, fontSize: 11),
-                          textAlign: TextAlign.center,
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
+            ),
+            Card(
+              color: Colors.black,
+              child: GridView.count(
+                crossAxisCount: 4,
+                shrinkWrap: true,
+                physics: const NeverScrollableScrollPhysics(),
+                mainAxisSpacing: 20,
+                crossAxisSpacing: 10,
+                children: const [
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(Icons.assignment, color: Colors.white, size: 28),
+                      SizedBox(height: 6),
+                      Text(
+                        "Assignment",
+                        style: TextStyle(color: Colors.white, fontSize: 11),
+                        textAlign: TextAlign.center,
+                      ),
+                    ],
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(Icons.schedule, color: Colors.white, size: 28),
+                      SizedBox(height: 6),
+                      Text(
+                        "Routine",
+                        style: TextStyle(color: Colors.white, fontSize: 11),
+                        textAlign: TextAlign.center,
+                      ),
+                    ],
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(Icons.local_library, color: Colors.white, size: 28),
+                      SizedBox(height: 6),
+                      Text(
+                        "Library",
+                        style: TextStyle(color: Colors.white, fontSize: 11),
+                        textAlign: TextAlign.center,
+                      ),
+                    ],
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(Icons.menu_book, color: Colors.white, size: 28),
+                      SizedBox(height: 6),
+                      Text(
+                        "E-Learning",
+                        style: TextStyle(color: Colors.white, fontSize: 11),
+                        textAlign: TextAlign.center,
+                      ),
+                    ],
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(Icons.shopping_bag, color: Colors.white, size: 28),
+                      SizedBox(height: 6),
+                      Text(
+                        "Leave",
+                        style: TextStyle(color: Colors.white, fontSize: 11),
+                        textAlign: TextAlign.center,
+                      ),
+                    ],
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(
+                        Icons.calendar_month_rounded,
+                        color: Colors.white,
+                        size: 28,
+                      ),
+                      SizedBox(height: 6),
+                      Text(
+                        "Calendar",
+                        style: TextStyle(color: Colors.white, fontSize: 11),
+                        textAlign: TextAlign.center,
+                      ),
+                    ],
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(
+                        Icons.bookmark_added_outlined,
+                        color: Colors.white,
+                        size: 28,
+                      ),
+                      SizedBox(height: 6),
+                      Text(
+                        "Syllabus",
+                        style: TextStyle(color: Colors.white, fontSize: 11),
+                        textAlign: TextAlign.center,
+                      ),
+                    ],
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(Icons.today, color: Colors.white, size: 28),
+                      SizedBox(height: 6),
+                      Text(
+                        "Daily Plan",
+                        style: TextStyle(color: Colors.white, fontSize: 11),
+                        textAlign: TextAlign.center,
+                      ),
+                    ],
+                  ),
+                ],
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
