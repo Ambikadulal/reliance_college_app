cb37bae8ec70ff1d4b1a79f682736df064213e81
Ambika <ambikadulal16@gmail.com>
2025-04-30 09:35:59 +0545

    appbar added

--- lib/pages/screens/my_home_page.dart
+++ lib/pages/screens/my_home_page.dart
@@ -62,9 +62,36 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            const DrawerHeader(
+              decoration: BoxDecoration(color: Colors.blue),
+              child: Text('Drawer Header'),
+            ),
+            ListTile(
+              title: const Text('Item 1'),
+              onTap: () {
+                // Handle item tap
+              },
+            ),
+            ListTile(
+              title: const Text('Item 2'),
+              onTap: () {
+                // Handle item tap
+              },
+            ),
+          ],
+        ),
+      ),
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
+
+        actions: [
+          IconButton(icon: const Icon(Icons.settings), onPressed: () async {}),
+        ],
       ),
       body: Center(child: _widgetOptions[_selectedIndex]),
 
